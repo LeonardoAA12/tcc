b3173f5121439cd9687db2c187a3cb92a5c53a24
LeonardoAA12 <leonardo.agostini2012@gmail.com>
2025-11-04 20:06:02 -0300

    Add files via upload

--- Forsaken.js
+++ Forsaken.js
@@ -2,7 +2,7 @@ function aleatorio(n){
     numero = Math.floor(Math.random() * n)+1
     document.getElementById('aleatorio').innerHTML = '+ '+numero+'$'
 }
-aleatorio(12500)
+aleatorio(15000)
 
 audio = document.getElementById('audio')
 audio.volume = 0.1
